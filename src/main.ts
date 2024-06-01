9488891f5bf93d7e18c9b0f3413344fabb4575ed
NAJIH Driss <driss.najih-ext@amundi.com>
2024-06-01 15:04:04 +0200

    migrate angular18

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {EntryComponent} from './entry.component';
 import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from "@angular/material/form-field";
 import {BrowserAnimationsModule, provideAnimations} from "@angular/platform-browser/animations";
-import {provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from "@angular/common/http";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatDialogModule} from "@angular/material/dialog";
 
